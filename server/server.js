43ed3552fc9afe888061720f7025d8c2fba7031d
HuiPan-Peter <132380239+HuiPan-Peter@users.noreply.github.com>
2024-01-08 15:59:50 -0400

    Adding Dependencies, Apollo Server and GraphQL query

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const path = require('path');
+
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+
+const { authMiddleware } = require("./utils/auth");
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
