8584fa28cc0c95c905747155e3aca5f341379111
HuiPan-Peter <132380239+HuiPan-Peter@users.noreply.github.com>
2024-01-10 16:25:58 -0400

    debuging and run test

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,20 +1,24 @@
+// Function to get saved book IDs from localStorage
 export const getSavedBookIds = () => {
+  // Retrieve saved book IDs from localStorage
   const savedBookIds = localStorage.getItem('saved_books')
     ? JSON.parse(localStorage.getItem('saved_books'))
     : [];
-
+  // Returns the array of saved book IDs
   return savedBookIds;
 };
-
+// Function to save book IDs to localStorage
 export const saveBookIds = (bookIdArr) => {
   if (bookIdArr.length) {
+    // Save the provided book IDs as a JSON string in localStorage
     localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
   } else {
     localStorage.removeItem('saved_books');
   }
 };
-
+// Function to remove a specific book ID from saved books in localStorage
 export const removeBookId = (bookId) => {
+  // Retrieve saved book IDs from localStorage, or set to null if not available
   const savedBookIds = localStorage.getItem('saved_books')
     ? JSON.parse(localStorage.getItem('saved_books'))
     : null;
@@ -22,8 +26,10 @@ export const removeBookId = (bookId) => {
   if (!savedBookIds) {
     return false;
   }
-
+  // Filter out the specified book ID from the saved book IDs array
   const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
+
+  // Save the updated book IDs to localStorage
   localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
 
   return true;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,101 +1,69 @@
-const { User } = require("../models");
-const { signToken, AuthenticationError } = require("../utils/auth");
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
-  Query: {
-    // Resolver for the 'me' query. Fetches user data if authenticated.
-    me: async (parent, args, context) => {
-      // Check if user is authenticated using context
-      if (context.user) {
-        // If authenticated, return user data
-        return User.findOne({ _id: context.user._id });
-      }
-      // If not authenticated, throw authentication error
-      throw AuthenticationError;
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({_id: context.user._id});
+            }
+        },
     },
-  },
-  Mutation: {
-    // Resolver for the 'login' mutation. Validates credentials and returns user data and token.
-    login: async (parent, { email, password }) => {
-      // Find user by email
-      const user = await User.findOne({ email });
 
-      // If user not found, throw authentication error
-      if (!user) {
-        throw AuthenticationError;
-      }
+    Mutation: {
+        addUser: async (_, {username, email, password}) => {
+            const user = await User.create({username, email, password});
+            console.log(user);
+            const token = signToken(user);
+          
+            return {token, user};
+        },
 
-      // Check if provided password is correct
-      const correctPw = await user.isCorrectPassword(password);
+        login: async (_, {username, email, password}) => {
+            const user = await User.findOne({ $or: [{ username: username }, { email: email }] });
 
-      // If incorrect password, throw authentication error
-      if (!correctPw) {
-        throw AuthenticationError;
-      }
+            if (!user) {
+                console.log("error: no user")
+                return;
+            }
 
-      // Sign a token for the authenticated user
-      const token = signToken(user);
+            const correctPw = await user.isCorrectPassword(password);
+            console.log(correctPw);
 
-      // Return token and user data
-      return { token, user };
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return {token, user};
+        },
+        saveBook: async (parent, {bookId, title, description, authors, image, link }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    {
+                        _id: context.user._id
+                    },
+                    {
+                        $addToSet: {
+                            savedBooks: { bookId: bookId, title: title, description: description, authors: authors, image: image, link: link},
+                        },
+                    },
+                    {new: true, runValidators: true},
+                );
+            }
+        },
+
+        removeBook: async (parent, {bookId}, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: {bookId: bookId }}},
+                    { new: true }
+                );
+            }
+        },
     },
-    // Resolver for the 'addUser' mutation. Creates a new user and returns user data and token.
-    addUser: async (parent, { username, email, password }) => {
-      // Creates a new user
-      const user = await User.create({ username, email, password });
-      // Sign a token for the new user
-      const token = signToken(user);
-      // Return token and user data
-      return { token, user };
-    },
-    // Resolver for the 'saveBook' mutation. Adds a new book to user's savedBooks list.
-    saveBook: async (
-      parent,
-      { authors, description, title, bookId, image, link },
-      context
-    ) => {
-      // Check if user is authenticated using context
-      if (context.user) {
-        // Add the new book to user's savedBooks list
-        const user = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          {
-            $addToSet: {
-              savedBooks: {
-                authors,
-                description,
-                title,
-                bookId,
-                image,
-                link,
-              },
-            },
-          },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-        // Return updated user data
-        return user;
-      }
-      // If user not authenticated, throw authentication error
-      throw AuthenticationError;
-    },
-    // Resolver for the 'removeBook' mutation. Removes a book from user's savedBooks list.
-    removeBook: async (parent, { bookId }, context) => {
-      // Check if user is authenticated using context
-      if (context.user) {
-        // Remove the specified book from user's savedBooks list
-        const user = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { savedBooks: { bookId } } },
-          { runValidators: true, new: true }
-        );
-        return user;
-      }
-    },
-  },
 };
 
 module.exports = resolvers;
