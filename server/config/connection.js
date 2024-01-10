5c9280c9dace4f711a903e60736d9289e416949c
HuiPan-Peter <132380239+HuiPan-Peter@users.noreply.github.com>
2024-01-10 19:55:19 -0400

    change db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 //Import mongoose
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/search-books');
 
 module.exports = mongoose.connection;
